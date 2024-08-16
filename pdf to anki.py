1f07b2d9cb94eabd596e4852058ce47c1b328e8d
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-16 21:32:14 +0200

    Add files via upload

--- pdf to anki.py
+++ pdf to anki.py
@@ -12,38 +12,35 @@ def main():
     text = page.extract_text()
     words_it = trans(text)
     audio_path= "audio.mp3"
-    x = create_audio(text, audio_path)
+    create_audio(text, audio_path)
     model_id = random.randrange(1 << 30, 1 << 31)
     deck_id = random.randrange(1 << 30, 1 << 31)
     my_model = genanki.Model(
         model_id,
         'Linguistics',
         fields=[
-        {'name': 'Question'},
-        {'name': 'Answer'},
-        {'name': 'MyMedia'}                               
+        {'name': 'Spanish'},
+        {'name': 'Italian'},
+        {'name': 'AudioFile'}                               
     ],
     templates=[
         {
         'name': 'Card 1',
-        'qfmt': '{{Question}}<br>{{[sound:sound.mp3]}}',              
-        'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+        'qfmt': '{{Spanish}}<br>{{AudioFile}}',              
+        'afmt': '{{FrontSide}}<hr id="answer">{{Italian}}',
         },
     ] )
-    print(text,words_it,deck_id,model_id)
     my_note= genanki.Note(
         model= my_model,
-        fields= [text, words_it,x]
+        fields= [text, words_it,audio_path]
     )
     my_deck = genanki.Deck(
         deck_id,
     'Espanol palabras')
     my_deck.add_note(my_note)
     try:
-        #my_package = genanki.Package(my_deck)
-        #my_package.media_files = ['sound.mp3', 'images/image.jpg']
-        #print(my_package.media_files)
         my_package = genanki.Package(my_deck)
+        my_package.media_files = ['audio.mp3']
         my_package.write_to_file("output.apkg")
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -62,8 +59,7 @@ def trans(text):
     translation= translator.translate(text, dest= "it")
     return translation.text
 
+#as of rn it doesnt create any problem when i run the program, still dont know where the created files end up but whatever
 
-#in this version i made a lot of changes, part of which were reverted to how it was. example is the fields part.Got like no idea on how to solve my current bugs
-#i guess ill just need to test everything out and figure something out
 
 main()
